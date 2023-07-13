37d0cc512657d4db00d43ce1a03341b35d92a0e1
JatinGhadi <127288506+JatinGhadi@users.noreply.github.com>
2023-07-13 14:55:05 +0530

    Add files via upload

---
+++ 12.07 js.js
@@ -0,0 +1,29 @@
+/*var a=10;
+document.write(a);
+document.write("<br>");
+let b=15;
+document.write(b);
+document.write("<br>");
+const c="Jatin";
+document.write(c);
+document.write("<br>");
+let a=prompt("Enter Name");
+document.write(a);*/
+function j()
+{
+    document.write("<h1>BCCI</h1>");
+}
+j();
+j();
+
+
+function add()
+{
+    var a=prompt("Enter number=1");
+    a=parseInt(a);
+    var b=prompt("Enter number=2");
+    b=parseInt(b);
+    alert(a+b);
+
+}
+add();
